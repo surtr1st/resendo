bbc879869c93d4aa6dbb8883d2b184518dab9413
Ishi Tran <berugen1st@gmail.com>
2023-03-05 18:07:39 +0700

    add list, update chatbox and container

--- client/src/components/Container/index.tsx
+++ client/src/components/Container/index.tsx
@@ -1,10 +1,24 @@
 import './style.css';
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
 };
+type GridItemProps = {
+  children: ReactNode;
+  col?: 'col-1' | 'col-2' | 'col-3' | 'col-4' | 'col-5' | 'col-6' | 'col-7' | 'col-8' | 'col-9' | 'col-10' | 'col-11' | 'col-12'
+  type: 'side' | 'article'
+}
 
-export const Container = ({ children }: Props) => (
-  <React.Fragment>{children}</React.Fragment>
-);
+export const Container = {
+  Grid: ({ children }: Props) => (
+    <div className="grid">
+      {children}
+    </div>
+  ),
+  GridItem: ({ children, col, type }: GridItemProps) => (
+    <div className={type}>
+      {children}
+    </div>
+  )
+};
