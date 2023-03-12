0a8bbcad055a3a793d197e47b5aca594d4f277cf
Ishi Tran <berugen1st@gmail.com>
2023-03-12 18:57:43 +0700

    add search and add friend

---
+++ client/src/components/User/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from '../Button'
+import './style.css'
+
+type Props = {
+  name: string
+  avatarSrc: string
+  addFriend: () => void | Promise<void>
+}
+
+export function User({ name, addFriend }: Partial<Props>) {
+  return (
+    <div className='user'>
+      <h4>{name}</h4>
+      <Button.Send label='Add' onSend={addFriend} />
+    </div>
+  )
+}
