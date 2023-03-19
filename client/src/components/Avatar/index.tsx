a44af46bde3e6bb90ef3c68f0e3b694b1a13be86
Ishi Tran <berugen1st@gmail.com>
2023-03-19 21:24:50 +0700

    add avatar component

--- client/src/components/Avatar/index.tsx
+++ client/src/components/Avatar/index.tsx
@@ -1,6 +1,25 @@
 import './style.css';
 
+type LabelProps = {
+  src: string
+  alt: string
+}
+
+type NoneLabelProps = {
+  name: string
+}
+
 export const Avatar = {
-  WithLabel: () => <div></div>,
-  WithoutLabel: () => <div></div>,
+  WithLabel: ({ src, alt }: LabelProps) => (
+    <div className='with-label'>
+      <img src={src} alt={alt} />
+    </div>
+  ),
+  WithoutLabel: ({ name }: NoneLabelProps) => (
+    <div className='without-label'>
+      <div className='none-label' title={name}>
+        <h3>{name.slice(0, 1).toUpperCase()}</h3>
+      </div>
+    </div>
+  )
 };
