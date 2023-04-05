f3b3d9c186b59dc3497a5bd92ce313ef6304ebce
Ishi Tran <berugen1st@gmail.com>
2023-04-05 13:03:08 +0700

    add dockerfile

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,15 +4,4 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      $: './src',
-      '$/components': './src/components/*',
-      '$/hooks': './src/hooks/index.ts',
-      '$/helpers': './src/helpers/index.ts',
-      '$/mixins': './src/mixins/*',
-      '$/types': './src/types/index.ts',
-      '$/views': './src/views/*',
-    },
-  },
 });
